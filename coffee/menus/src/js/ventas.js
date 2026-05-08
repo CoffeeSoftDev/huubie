3b468d45e18516d22c98fb45dba46eeb9976788f
SomxD <desarrollo080293@gmail.com>
2026-05-08 17:14:20 -0600

    feat: ventas

--- coffee/menus/src/js/ventas.js
+++ coffee/menus/src/js/ventas.js
@@ -1,6 +1,6 @@
 // Instanciar la clase y generar las tarjetas
 $(async () => {
-    const data = await useFetch({ url: "/alpha/access/ctrl/ctrl-access.php", data: { opc: 'company' } });
+    const data = await useFetch({ url: "/coffee/access/ctrl/ctrl-access.php", data: { opc: 'company' } });
 
     let tarjetas = [];
     // Crear las tarjetas dinamicamente
@@ -9,16 +9,16 @@ $(async () => {
             tarjetas.push({
                 titulo: route['name'],
                 descripcion: route['description'],
-                imagen: "/alpha/" + route['image'],
-                enlace: "/alpha/" + route['route'],
+                imagen: "/coffee/" + route['image'],
+                enlace: "/coffee/" + route['route'],
                 padding: route['padding']
             });
         } else {
             tarjetas.push({
                 titulo: route['nickname'],
                 descripcion: route['description'],
-                imagen: "/alpha/" + route['image'],
-                enlace: "/alpha/" + route['route'],
+                imagen: "/coffee/" + route['image'],
+                enlace: "/coffee/" + route['route'],
                 padding: route['padding']
             });
         }
@@ -28,22 +28,22 @@ $(async () => {
         tarjetas.push({
             titulo: "Reportes",
             descripcion: "Genera reportes detallados de ventas y turnos.",
-            imagen: "/alpha/src/img/reportes.svg",
-            enlace: "/alpha/pedidos-reportes/",
+            imagen: "/coffee/src/img/reportes.svg",
+            enlace: "/coffee/pedidos-reportes/",
             padding: ""
         });
         // tarjetas.push({
         //     titulo: "Inventario",
         //     descripcion: "Administra tu inventario de productos.",
-        //     imagen: "/alpha/src/img/inventario.svg",
-        //     enlace: "/alpha/inventario/",
+        //     imagen: "/coffee/src/img/inventario.svg",
+        //     enlace: "/coffee/inventario/",
         //     padding: ""
         // });
         tarjetas.push({
             titulo: "Configuración",
             descripcion: "Agrega usuarios y sucursales.",
-            imagen: "/alpha/src/img/configuracion.svg",
-            enlace: "/alpha/admin/",
+            imagen: "/coffee/src/img/configuracion.svg",
+            enlace: "/coffee/admin/",
             padding: ""
         });
     }
--- coffee/menus/ventas.php
+++ coffee/menus/ventas.php
@@ -1,44 +1,41 @@
 <?php require_once("../conf/_Rutes.php"); ?>
 <!DOCTYPE html>
-<html lang="es">
+<html class="bg-[#111928]" lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/svg+xml" href="<?=PATH_BASE?>src/img/logo/logo.ico" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="/coffee/">
+    <link rel="icon" type="image/svg+xml" href="src/img/logo/logo.ico" />
     <title>Alpha</title>
-    <link rel="stylesheet" href="<?=PATH_BASE?>src/plugins/fontello/css/fontello.css">
-    <link rel="stylesheet" href="<?=PATH_BASE?>src/plugins/fontello/css/animation.css">
-    
-    <link rel="stylesheet" href="<?=PATH_BASE?>src/plugins/sweetalert2/sweetalert2.min.css" />
-    <script src="<?=PATH_BASE?>src/plugins/sweetalert2/sweetalert2.all.min.js"></script>
-    
-    <link rel="stylesheet" href="<?=PATH_BASE?>src/plugins/bootstrap-5/css/bootstrap.min.css" />
 
-    <script src="<?=PATH_BASE?>src/plugins/jquery/jquery-3.7.0.js"></script>
-    <script src="<?=PATH_BASE?>src/plugins/bootstrap-5/js/bootstrap.bundle.js"></script>
-    <script src="<?=PATH_BASE?>src/js/complementos.js"></script>
-    <script src="<?=PATH_BASE?>src/js/plugins.js"></script>
-    <script src="<?=PATH_BASE?>src/js/coffeSoft.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <?php require_once(__DIR__ . '/../layout/head.php'); ?>
+    <?php require_once(__DIR__ . '/../layout/core-libraries.php'); ?>
+
+    <!-- CoffeeSoft Framework -->
+    <script src="src/js/coffeeSoft.js?t=<?php echo time(); ?>"></script>
+    <script src="src/js/plugins.js?t=<?php echo time(); ?>"></script>
+    <script src="src/js/complementos.js"></script>
 </head>
 
-<body class="bg-[#111928]">
+<body class="bg-[#111928] h-screen" data-bs-theme="dark">
     <div id="menu-navbar"></div>
     <div id="menu-sidebar"></div>
-    <div id="mainContainer"
-        class="w-full h-[calc(100vh-3rem)]  bg-[#111928] mt-5 pt-5 transition-all duration-500  text-white p-6 flex flex-col md:flex-row gap-x-4">
-        <div id="grid-card" class="w-full grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-2"></div>
-    </div>
-    <script src="<?= PATH_BASE ?>access/src/js/session.js?t=<?= time(); ?>"></script>
-    <script src="<?=PATH_MENU?>src/js/navbar.js"></script>
-    <script src="<?=PATH_MENU?>src/js/sidebar.js"></script>
-    <script src="<?=PATH_MENU?>src/js/cards.js?t=<?= time() ?>"></script>
-    <script src="<?=PATH_MENU?>src/js/ventas.js?t=<?= time() ?>"></script>
-</body>
-<script>
 
-</script>
+    <div id="mainContainer"
+        class="w-full h-[calc(100vh-3rem)] bg-[#111928] pt-5 transition-all duration-500 text-white p-6 flex flex-col md:flex-row gap-x-4">
+        <div id="grid-card"
+            class="w-full grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2"></div>
+    </div>
+
+    <!-- Navbar & Sidebar -->
+    <script src="access/src/js/session.js"></script>
+    <script src="src/js/navbar.js"></script>
+    <script src="src/js/sidebar.js"></script>
+
+    <!-- Modulo Scripts -->
+    <script src="menus/src/js/cards.js?t=<?php echo time(); ?>"></script>
+    <script src="menus/src/js/ventas.js?t=<?php echo time(); ?>"></script>
+</body>
 
 </html>
